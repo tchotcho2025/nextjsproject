a9b388c7ad487865def6e2a0bf74f1a0980b43b3
tchotcho2025 <tchotcho_de@hotmail.com>
2025-02-24 11:05:43 -0500

    meetup project ready for next deployment

---
+++ meetup-project/.next/server/pages/api/new-meetup.js
@@ -0,0 +1,56 @@
+"use strict";
+(() => {
+var exports = {};
+exports.id = 958;
+exports.ids = [958];
+exports.modules = {
+
+/***/ 5940:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "default": () => (/* binding */ new_meetup)
+});
+
+;// CONCATENATED MODULE: external "mongodb"
+const external_mongodb_namespaceObject = require("mongodb");
+;// CONCATENATED MODULE: ./pages/api/new-meetup.js
+
+// /api/new-meetup
+// POST /api/new-meetup
+async function handler(req, res) {
+    if (req.method === "POST") {
+        const data = req.body;
+        //const { title, image, address, description } = data;
+        //MongoClient.connect('mongodb+srv://hugodubled:<db_password>@cluster0.89odw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+        const client = await external_mongodb_namespaceObject.MongoClient.connect("mongodb+srv://hugodubled:VNuHdvlg1mvABOjy@cluster0.89odw.mongodb.net/meetups?retryWrites=true&w=majority&appName=Cluster0");
+        const db = client.db();
+        const meetupsCollection = db.collection("meetups");
+        const result = await meetupsCollection.insertOne(data);
+        console.log(result);
+        client.close();
+        res.status(201).json({
+            message: "Meetup inserted"
+        });
+    }
+}
+/* harmony default export */ const new_meetup = (handler);
+
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../../webpack-api-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = (__webpack_exec__(5940));
+module.exports = __webpack_exports__;
+
+})();
